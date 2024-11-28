89ac165287f1bee255859779a26bb73504da1b71
PraWater <praneel.maddula@gmail.com>
2024-11-28 14:04:59 +0530

    add: draw pixels

--- chip8.cpp
+++ chip8.cpp
@@ -49,103 +49,178 @@ chip8::chip8() {
   for (int i = 0; i < 80; ++i) {
     memory[i] = fontset[i];
   }
+  drawFlag = true;
+
+  srand(time(NULL));
 }
 
 chip8::~chip8() { cout << "Bye" << endl; }
 
 void chip8::emulateCycle() {
   opcode = memory[pc] << 8 | memory[pc + 1];
+  /*printf("pc: %d, opcode: 0x%x\n", pc, opcode);*/
   switch (opcode & 0xF000) {
-  case 0x1000: // 1xxx calls jump to xxx.
-    pc = opcode & 0x0FFF;
-    break;
-  case 0x2000: // 2xxx calls subroutine at xxx.
-    stack[sp] = pc;
-    ++sp;
-    pc = opcode & 0x0FFF;
-    break;
-  case 0x3000: // 3xkk - if Vx == kk, next instruction is skipped.
-    if (V[(opcode & 0x0F00) >> 8] == (opcode & 0x00FF))
-      pc += 2;
-    pc += 2;
-    break;
-  case 0x4000: // 4xkk - if Vx != kk, next instruction is skipped.
-    if (V[(opcode & 0x0F00) >> 8] != (opcode & 0x00FF))
-      pc += 2;
-    pc += 2;
-    break;
-  case 0x5000: // 5xy0 - if Vx == Vy, next instruction is skipped.
-    if (V[(opcode & 0x0F00) >> 8] == V[(opcode & 0x00F0) >> 4])
-      pc += 2;
-    pc += 2;
-    break;
-  case 0x6000: // 6xkk puts kk into Vx
-    V[(opcode & 0x0F00) >> 8] = opcode & 0x00FF;
-    pc += 2;
-    break;
-  case 0x7000: // 7xkk adds kk to Vx
-    V[(opcode & 0x0F00) >> 8] += opcode & 0x00FF;
-    pc += 2;
-    break;
-  case 0x8000:
+
+  case 0x0000: // 1xxx calls jump to xxx.
     switch (opcode & 0x000F) {
-    case 0x0000: // 8xy0 - set Vx = Vy.
-      V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x00F0) >> 4];
+    case 0x0000: // 00E0 clears the screen.
+      for (int i = 0; i < 2048; ++i)
+        gfx[i] = false;
+      drawFlag = true;
       pc += 2;
       break;
-    case 0x0001: // 8xy1 - set Vx = Vx | Vy.
-      V[(opcode & 0x0F00) >> 8] =
-          V[(opcode & 0x0F00) >> 8] | V[(opcode & 0x00F0) >> 4];
-      pc += 2;
+
+    case 0x000E: // 00EE returns from subroutine.
+      --sp;
+      pc = stack[sp];
       break;
-    case 0x0002: // 8xy1 - set Vx = Vx & Vy.
-      V[(opcode & 0x0F00) >> 8] =
-          V[(opcode & 0x0F00) >> 8] & V[(opcode & 0x00F0) >> 4];
-      pc += 2;
-      break;
-    case 0x0003: // 8xy1 - set Vx = Vx ^ Vy.
-      V[(opcode & 0x0F00) >> 8] =
-          V[(opcode & 0x0F00) >> 8] ^ V[(opcode & 0x00F0) >> 4];
-      pc += 2;
-      break;
+
     default:
       printf("Opcode not handled yet: 0x%X\n", opcode);
       exit(1);
       break;
     }
     break;
+
+  case 0x1000: // 1xxx calls jump to xxx.
+    pc = opcode & 0x0FFF;
+    break;
+
+  case 0x2000: // 2xxx calls subroutine at xxx.
+    stack[sp] = pc;
+    ++sp;
+    pc = opcode & 0x0FFF;
+    break;
+
+  case 0x3000: // 3xkk - if Vx == kk, next instruction is skipped.
+    if (V[(opcode & 0x0F00) >> 8] == (opcode & 0x00FF))
+      pc += 2;
+    pc += 2;
+    break;
+
+  case 0x4000: // 4xkk - if Vx != kk, next instruction is skipped.
+    if (V[(opcode & 0x0F00) >> 8] != (opcode & 0x00FF))
+      pc += 2;
+    pc += 2;
+    break;
+
+  case 0x5000: // 5xy0 - if Vx == Vy, next instruction is skipped.
+    if (V[(opcode & 0x0F00) >> 8] == V[(opcode & 0x00F0) >> 4])
+      pc += 2;
+    pc += 2;
+    break;
+
+  case 0x6000: // 6xkk puts kk into Vx
+    V[(opcode & 0x0F00) >> 8] = opcode & 0x00FF;
+    pc += 2;
+    break;
+
+  case 0x7000: // 7xkk adds kk to Vx
+    V[(opcode & 0x0F00) >> 8] += opcode & 0x00FF;
+    pc += 2;
+    break;
+
+  case 0x8000:
+    switch (opcode & 0x000F) {
+
+    case 0x0000: // 8xy0 - set Vx = Vy.
+      V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x00F0) >> 4];
+      pc += 2;
+      break;
+
+    case 0x0001: // 8xy1 - set Vx = Vx | Vy.
+      V[(opcode & 0x0F00) >> 8] =
+          V[(opcode & 0x0F00) >> 8] | V[(opcode & 0x00F0) >> 4];
+      pc += 2;
+      break;
+
+    case 0x0002: // 8xy1 - set Vx = Vx & Vy.
+      V[(opcode & 0x0F00) >> 8] =
+          V[(opcode & 0x0F00) >> 8] & V[(opcode & 0x00F0) >> 4];
+      pc += 2;
+      break;
+
+    case 0x0003: // 8xy1 - set Vx = Vx ^ Vy.
+      V[(opcode & 0x0F00) >> 8] =
+          V[(opcode & 0x0F00) >> 8] ^ V[(opcode & 0x00F0) >> 4];
+      pc += 2;
+      break;
+
+    default:
+      printf("Opcode not handled yet: 0x%X\n", opcode);
+      exit(1);
+      break;
+    }
+    break;
+
   case 0x9000: // 9xy0 - if Vx != Vy, next instruction is skipped.
     if (V[(opcode & 0x0F00) >> 8] != V[(opcode & 0x00F0) >> 4])
       pc += 2;
     pc += 2;
     break;
+
   case 0xA000: // Axxx moves I to xxx.
     I = opcode & 0x0FFF;
     pc += 2;
     break;
+
   case 0xB000: // Bxxx moves pc to xxx + V0.
     pc = opcode & 0x0FFF;
     pc += V[0];
     break;
+
   case 0xC000: // Cxkk moves pc to xxx + V0.
-    srand(time(0));
     V[(opcode & 0x0F00) >> 8] = (opcode & 0x00FF) & (rand() % 256);
     pc += 2;
     break;
+
+  case 0xD000: // Dxyn - draws n bytes starting from I at (Vx, Vy).
+  {
+    unsigned short x = V[(opcode & 0x0F00) >> 8];
+    unsigned short y = V[(opcode & 0x00F0) >> 4];
+    unsigned short height = opcode & 0x000F;
+    unsigned short pixel;
+
+    V[0xF] = 0;
+    for (int yline = 0; yline < height; yline++) {
+      pixel = memory[I + yline];
+      for (int xline = 0; xline < 8; xline++) {
+        if ((pixel & (0x80 >> xline)) != 0) {
+          if (gfx[(x + xline + ((y + yline) * 64))] == 1) {
+            V[0xF] = 1;
+          }
+          gfx[x + xline + ((y + yline) * 64)] ^= 1;
+        }
+      }
+    }
+    drawFlag = true;
+    pc += 2;
+  } break;
+
   case 0xF000:
     switch (opcode & 0x00FF) {
-    case 0x0007: // Fx07 will place delay timer value in Vx
+    case 0x0007: // Fx07 will place delay timer value in Vx.
       V[(opcode & 0x0F00) >> 8] = delay_timer;
       pc += 2;
       break;
+
+    case 0x001E: // Fx1E will add Vx to I.
+      I += V[(opcode & 0x0F00) >> 8];
+      pc += 2;
+      break;
+
+    default:
+      printf("Opcode at pc: %d not handled yet: 0x%X\n", pc, opcode);
+      exit(1);
+      break;
     }
     break;
+
   default:
-    printf("Opcode not handled yet: 0x%X\n", opcode);
+    printf("Opcode at pc: %d not handled yet: 0x%X\n", pc, opcode);
     exit(1);
     break;
   }
-  pc += 2;
 }
 
 bool chip8::loadApplication(const char *filename) {
--- main.cpp
+++ main.cpp
@@ -1,13 +1,29 @@
 #include "chip8.h"
+#include <cstdio>
+
+void drawPixels(chip8* ch) {
+  for (int i = 0; i < 32; ++i) {
+    for (int j = i * 64; j < (i + 1) * 64; ++j) {
+      if (ch->gfx[j])
+        printf("$$");
+      else
+        printf("  ");
+    }
+    printf("\n");
+  }
+}
 
 int main() {
   chip8 ch = chip8();
-
-  ch.loadApplication("pong2.c8");
+  ch.loadApplication("invaders.c8");
 
   while (1) {
     ch.emulateCycle();
-    //TODO draw pixels on screen.
+    if (ch.drawFlag) {
+      drawPixels(&ch);
+      /*printf("drawing\n");*/
+      ch.drawFlag = false;
+    }
   }
   return 0;
 }
